5012aee92e5f549ea9e15b3c3bea063ba36dd982
DekApps13 <derekvillarreal60@gmail.com>
2024-07-07 14:53:57 -0500

    create diaries route

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,22 @@
-import express from "express";
+import express from 'express';
+import diariesRouter from './routes/diaries';
+
 const app = express();
 app.use(express.json()); // Middleware que transforma la req.body a json
 
 const PORT = 3000;
 
-app.get("/", (_req, res) => {
-  console.log("Home page");
-  res.status(200).json({ message: "Hola Derek" });
-});
+app.get('/', (_req, res) => {
+  console.log('Home page');
+  res.status(200).json({ message: 'Hola Derek' });
+})
 
-app.get("/ping", (_req, res) => {
-  console.log("Someone pinged here!!!");
-  res.status(200).send("pong");
-});
+app.get('/ping', (_req, res) => {
+  console.log('Someone pinged here!!!');
+  res.status(200).send('pong');
+})
+
+app.use('/api/diaries', diariesRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
